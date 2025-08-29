fe06668f0e393d61abb853687ed72dc27ac32320
mohdathar42 <mohdatharkhan42@gmail.com>
2025-08-30 03:01:09 +0530

    playing with multiple params

--- src/app.js
+++ src/app.js
@@ -3,20 +3,12 @@ const app = express();
 const port = 777;
 
  
-app.get("/user", (req, res) => {
+app.get("/user/:userId/:name/:password", (req, res) => {
+  console.log(req.params)
   res.send({firstname:"mohd",lastname:"athar" ,class:"mca"});
 });
  
-app.post("/user", (req, res) => { 
-console.log("hello");
-
-  res.send( "data succesfully saved in the database");
-});
  
-app.delete("/user", (req, res) => {
-  res.send( "user successfully delete from the db")
-});
-
 app.listen(port, () => {
   console.log(`we are listening port no  ${port}`);
 });
