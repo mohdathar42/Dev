ee1272adb84c3eefd2d94c63a47f75f0d5d02fca
mohdathar42 <mohdatharkhan42@gmail.com>
2025-09-17 22:31:04 +0530

    schema validATION

--- src/app.js
+++ src/app.js
@@ -11,13 +11,71 @@ app.post("/signUp", async (req, res) => {
   const user = new User(req.body);
   try {
     //save the data into database
-     await user.save();
-    res.send("user added successfully");
+    const addUser = await user.save();
+    res.send(`user added successfully and the user is 
+      ${addUser}`);
   } catch (err) {
-    res.status(400).send("error saving the user" + err.message);
+    res.status(400).send("error saving the user:     " + err.message);
   }
 });
 
+app.get("/getOneUser", async (req, res) => {
+  const userEmail = req.body.emailId;
+  try {
+    //get one user from db
+    const user = await User.find({ emailId: userEmail });
+    if (user.length === 0) {
+      res.status(404).send("something went wrong");
+    } else {
+      res.send(user);
+    }
+  } catch (err) {
+    res.status(400).send(" error in get all user api " + err.message);
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    //get all user from db
+    const allUsers = await User.find({});
+    if (allUsers.length === 0) {
+      res.status(404).send("there is no user in the db");
+    } else {
+      res.send(allUsers);
+    }
+  }catch (err) {
+    res.status(400).send(" error in get all user api " + err.message);
+  }
+});
+
+app.delete("/deleteUser", async (req, res) => {
+     const userId = req.body.userId;
+  try {
+  
+    //delete a user from db
+    const deleteUser = await User.findByIdAndDelete(userId);
+    if (!deleteUser) {
+      res.status(404).send(`there is no user in the db to delete " ${userId} "  `);
+    } else {
+      res.send("user delete successfully"+deleteUser);
+    }
+  } catch (err) {
+    res.status(400).send(" error in get delete user api " + err.message);
+  }
+});
+
+
+
+app.patch("/updateUser",async(req,res)=>{
+  const userId=req.body.userId;
+  const data=req.body;
+  try{
+const user=await User.findByIdAndUpdate({_id:userId},data,{runValidators:true})
+res.send(`user update successfully   ${user}`)
+  }catch(err){
+    res.status(400).send("UPDATE FAILED   "+err.message)
+  }
+})
 //connect database
 connectDb()
   .then(() => {
--- src/models/user.js
+++ src/models/user.js
@@ -1,25 +1,64 @@
 const mongoose = require("mongoose");
 
-const userSchema = mongoose.Schema({
-  firstName: {
-    type: String,
+const userSchema = mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+       required:true,
+      lowercase:true,
+      trim:true,
+      minLength: 4,
+    },
+    lastName: {
+      type: String,
+      required:true,
+      lowercase:true,
+      trim:true,
+      minLength:4
+    },
+    emailId: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      min: 18,
+      max:50
+    },
+    gender: {
+      type: String,
+      //custom validation function
+      validate(value) {
+        if (!["male", "female", "other"].includes(value)) {
+          throw new Error("gender data is not valid");
+        }
+      },
+    },
+    photoUrl: {
+      type: String,
+      default:
+        "https://www.un.org/pga/wp-content/uploads/sites/53/2018/09/Dummy-image-1.jpg",
+    },
+    about: {
+      type: String,
+      default: "this is nthe description of user",
+    },
+    skills: {
+      type: [String],
+      default: ["javascript", "node js", "python"],
+    },
   },
-  lastName: {
-    type: String,
-  },
-  email: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  gender: {
-    type: String,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
-const User = mongoose.model("User",userSchema);
+const User = mongoose.model("User", userSchema);
 module.exports = User;
